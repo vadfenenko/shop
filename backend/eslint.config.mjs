ac9ba8df988f7c8b9fa1f56464de6e49d4b8fd6e
fnnk <vadfenenko@gmail.com>
2025-03-19 21:22:32 +0100

    add ESLint configuration file (eslint.config.mjs)

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import globals from "globals";
+
+export default [
+ 
+  js.configs.recommended,
+  
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+      globals: globals.node,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "@typescript-eslint/no-unused-vars": "warn"
+    }
+  }
+];
